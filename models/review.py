d5c2d6d57afa9fda46ec5178fd3fa1104c520795
nja93 <njanjalchege@gmail.com>
2023-08-23 15:33:00 -0700

    no. 9

--- models/review.py
+++ models/review.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""This module defines a Review class"""
+""" Review module for the HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, ForeignKey
+import models
 
-from models.base_model import BaseModel
-
-
-class Review(BaseModel):
-    """Class is responsible for review objects"""
-
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review class to store review information """
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'),nullable=False)
+    text = Column(String(1024), nullable=False)
